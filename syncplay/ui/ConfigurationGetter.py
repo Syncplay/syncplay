252960159e5875c6f8a32296be5683b600ce45c7
Uriziel <urizieli@gmail.com>
2012-10-16 17:16:34 +0200

    Fix for finding configuration file

--- syncplay/ui/ConfigurationGetter.py
+++ syncplay/ui/ConfigurationGetter.py
@@ -20,7 +20,7 @@ class ConfigurationGetter(object):
     def _findWorkingDir(self):
         frozen = getattr(sys, 'frozen', '')
         if not frozen:
-            path = os.path.dirname(os.path.dirname(__file__))
+            path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
         elif frozen in ('dll', 'console_exe', 'windows_exe'):
             path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
         else:
