ec176eaf054a9bad83573cc8942b9de402e02143
Uriziel <urizieli@gmail.com>
2012-12-10 23:13:48 +0100

    Fix on choosing ports for server

--- syncplayServer.py
+++ syncplayServer.py
@@ -7,5 +7,5 @@ from syncplay.ui.ConfigurationGetter import ServerConfigurationGetter
  
 argsGetter = ServerConfigurationGetter()
 args = argsGetter.getConfiguration()
-reactor.listenTCP(args.port, SyncFactory(args.password, args.isolate_rooms))
+reactor.listenTCP(int(args.port), SyncFactory(args.password, args.isolate_rooms))
 reactor.run()
