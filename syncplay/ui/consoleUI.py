192153ab630475610ac535f25a3d36448986759f
Etoh <boldly@gmail.com>
2012-12-27 14:31:09 +0000

    Update consoleUI.py - Allow / sign for rel offset

--- syncplay/ui/consoleUI.py
+++ syncplay/ui/consoleUI.py
@@ -59,7 +59,7 @@ class ConsoleUI(threading.Thread):
             return None
         
     def _tryAdvancedCommands(self, data):
-        o = re.match(r"^(?:o|offset)\ ?(?P<sign>[+-])?(?P<time>\d+(?:[^\d\.](?:\d+)){0,2}(?:\.(?:\d+))?)$", data)
+        o = re.match(r"^(?:o|offset)\ ?(?P<sign>[/+-])?(?P<time>\d+(?:[^\d\.](?:\d+)){0,2}(?:\.(?:\d+))?)$", data)
         s = re.match(r"^(?:s|seek)?\ ?(?P<sign>[+-])?(?P<time>\d+(?:[^\d\.](?:\d+)){0,2}(?:\.(?:\d+))?)$", data)
         if(o):
             sign = self._extractSign(o.group('sign'))
