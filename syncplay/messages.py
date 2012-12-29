61ce6599604aeb97cd4fb1ddc17bbed9d8a52605
Et0h <boldly@gmail.com>
2012-12-29 13:23:39 +0000

    Move client.py messages to messages.py

--- syncplay/messages.py
+++ syncplay/messages.py
@@ -1,5 +1,38 @@
 en = {
-      "connecting" : "Attempting to connect to {}:{}"
+     
+      # Client notifications
+      "connection-attempt-notification" : "Attempting to connect to {}:{}", #Port, IP
+      "reconnection-attempt-notification" : "Connection with server lost, attempting to reconnect",
+      "disconnection-notification" : "Disconnected from server",
+      "connection-failed-notification" : "Connection with server failed",
+      
+      "rewind-notification" : "Rewinded due to time difference with <{}>", #User
+      "slowdown-notification" : "Slowing down due to time difference with <{}>", #User
+      "revert-notification" : "Reverting speed back to normal", 
+      
+      "pause-notification" : "<{}> paused", #User
+      "unpause-notification" : "<{}> unpaused", #User
+      "seek-notification" : "<{}> jumped from {} to {}", #User, from time, to time
+      
+      "current-offset-notification" : "Current offset: {} seconds", #Offset
+      
+      "room-join-notification" : "<{}> has joined the room: '{}'", #User
+      "left-notification" : "<{}> has left", #User
+      "playing-notification" : "<{}> is playing '{}' ({})", #User, file, duration
+      "playing-notification/room-addendum" :  " in room: '{}'", #Room
+      
+      "file-different-notification" : "File you are playing appears to be different from <{}>'s", #User
+      "file-differences-notification" : "Your file differs in the following way(s): ",
+      
+      "different-filesize-notification" : " (their file size is different from yours!)",
+      "file-played-by-notification" : "File: {} is being played by:", #User
+      "notplaying-notification" : "People who are not playing any file:",
+      
+      # Client prompts
+      "enter-to-exit-prompt" : "Press enter to exit\n",
+      
+      # Client errors
+      "server-timeout-error" : "Connection with server timed out"
       }
 
 messages = {
