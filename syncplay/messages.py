e2f08b6df74deb7958044b391bae79cd02aa62d6
Uriziel <urizieli@gmail.com>
2012-12-29 12:10:14 +0100

    New method for getting messages

--- syncplay/messages.py
+++ syncplay/messages.py
@@ -5,3 +5,12 @@ en = {
 messages = {
            "en": en
            }
+
+def getMessage(locale, type_):
+    if(messages.has_key(locale)):
+        if(messages[locale].has_key(type_)):
+            return messages[locale][type_]
+        elif(messages["en"].has_key(type_)):
+            return messages["en"][type_]
+    else:
+        raise KeyError()
