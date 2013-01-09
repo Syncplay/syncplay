af8ee57cd2852249da4dbd0082b6cf6d32eacffe
Uriziel <urizieli@gmail.com>
2013-01-09 21:27:59 +0100

    Documented basePlayer interface a bit

--- syncplay/players/basePlayer.py
+++ syncplay/players/basePlayer.py
@@ -1,23 +1,47 @@
 from syncplay import constants
 class BasePlayer(object):
+  
+    '''
+    This method is supposed to 
+    execute updatePlayerStatus(paused, position) on client
+    Given the arguments: boolean paused and float position in seconds 
+    '''
     def askForStatus(self):
         raise NotImplementedError()
 
+    '''
+    Display given message on player's OSD or similar means
+    '''
     def displayMessage(self, message, duration = constants.OSD_DURATION):
         raise NotImplementedError()
 
+    '''
+    Cleanup connection with player before syncplay will close down
+    '''
     def drop(self):
         raise NotImplementedError()
 
+    '''
+    Start up the player, returns its instance
+    '''
     @staticmethod
     def run(client, playerPath, filePath, args):
         raise NotImplementedError()
 
+    '''
+    @type value: boolean 
+    '''
     def setPaused(self, value):
         raise NotImplementedError()
 
+    '''
+    @type value: float 
+    '''
     def setPosition(self, value):
         raise NotImplementedError()
 
+    '''
+    @type value: float 
+    '''
     def setSpeed(self, value):
         raise NotImplementedError()
