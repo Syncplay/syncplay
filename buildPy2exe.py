2ec433f400fa86ae67e9a7dd3d97591b8a5bf1a0
Etoh <boldly@gmail.com>
2013-12-28 21:26:57 +0000

    Exclude Tkinter from build

--- buildPy2exe.py
+++ buildPy2exe.py
@@ -539,7 +539,7 @@ info = dict(
                          'dist_dir': OUT_DIR,
                          'packages': 'PySide.QtUiTools',
                          'includes': 'twisted, sys, encodings, datetime, os, time, math, PySide',
-                         'excludes': 'venv, _ssl, doctest, pdb, unittest, win32clipboard, win32file, win32pdh, win32security, win32trace, win32ui, winxpgui, win32pipe, win32process',
+                         'excludes': 'venv, _ssl, doctest, pdb, unittest, win32clipboard, win32file, win32pdh, win32security, win32trace, win32ui, winxpgui, win32pipe, win32process, Tkinter',
                          'dll_excludes': 'msvcr71.dll, MSVCP90.dll, POWRPROF.dll',
                          'optimize': 2,
                          'compressed': 1
