cabea05a2f4ff8540567d86d1dfa29e4d7ae7652
Niklas Haas <git@nand.wakku.to>
2014-03-27 02:11:06 +0100

    Add noGui to the list of configurable options

--- syncplay/ui/ConfigurationGetter.py
+++ syncplay/ui/ConfigurationGetter.py
@@ -66,7 +66,7 @@ class ConfigurationGetter(object):
 
         self._iniStructure = {
                         "server_data": ["host", "port", "password"],
-                        "client_settings": ["name", "room", "playerPath", "slowOnDesync", "rewindOnDesync", "forceGuiPrompt", "filenamePrivacyMode", "filesizePrivacyMode", "pauseOnLeave"],
+                        "client_settings": ["name", "room", "playerPath", "slowOnDesync", "rewindOnDesync", "forceGuiPrompt", "filenamePrivacyMode", "filesizePrivacyMode", "pauseOnLeave", "noGui"],
                         }
 
         #
