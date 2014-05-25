7d564b537b88e4b06e96c64b327930b2761e0509
Et0h <boldly@gmail.com>
2014-05-25 02:14:35 +0100

    Delete random True

--- syncplay/constants.py
+++ syncplay/constants.py
@@ -34,7 +34,6 @@ SHOW_REWIND_ON_DESYNC_CHECKBOX = False
 SHOW_TOOLTIPS = True
 MERGE_PLAYPAUSE_BUTTONS = False
 SYNC_ON_PAUSE = True # Client seek to global position - subtitles may disappear on some media players
-True
 #Usually there's no need to adjust these
 FILENAME_STRIP_REGEX = u"[-~_\.\[\](): ]" 
 COMMANDS_UNDO = ["u", "undo", "revert"]
