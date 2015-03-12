415ceffc6df2e0add726be31a3da52427128ccab
Et0h <boldly@gmail.com>
2015-03-12 08:58:45 +0000

    Use sys.stderr over subprocess.STD_ERROR_HANDLE as it should be cross-platform compatible

--- syncplay/players/vlc.py
+++ syncplay/players/vlc.py
@@ -311,10 +311,7 @@ class VlcPlayer(BasePlayer):
                     elif "lua interface error" in line:
                         playerController._client.ui.showErrorMessage(getMessage("media-player-error").format(line), True)
                         break
-            try:
-                self.__process.stderr = subprocess.STD_ERROR_HANDLE
-            except:
-                self.__playerController._client.ui.showDebugMessage("Failed to set VLC's stderr to STD_ERROR_HANDLE")
+            self.__process.stderr = sys.stderr
             threading.Thread.__init__(self, name="VLC Listener")
             asynchat.async_chat.__init__(self)
             self.set_terminator("\n")
