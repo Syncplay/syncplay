894f9ddee685d1cc114c5613531ec51fd1f01bb6
Alberto Sottile <alby128@gmail.com>
2019-02-03 16:40:50 +0100

    TLS: server listens on TLS via IPv4

--- syncplayServer.py
+++ syncplayServer.py
@@ -13,10 +13,24 @@ except AttributeError:
     import warnings
     warnings.warn("You must run Syncplay with Python 3.4 or newer!")
 
-from twisted.internet import reactor
-from twisted.internet.endpoints import TCP4ServerEndpoint, TCP6ServerEndpoint
+from OpenSSL import crypto
+from twisted.internet import reactor, ssl
+from twisted.internet.endpoints import TCP4ServerEndpoint, SSL4ServerEndpoint, TCP6ServerEndpoint
 
 from syncplay.server import SyncFactory, ConfigurationGetter
+
+with open('server.crt', 'r') as f:
+    cert_data = f.read()
+with open('server.key', 'r') as f:
+    key_data = f.read()
+
+cert = crypto.load_certificate(crypto.FILETYPE_PEM, cert_data)
+key = crypto.load_privatekey(crypto.FILETYPE_PEM, key_data)
+options = ssl.CertificateOptions(
+    privateKey=key,
+    certificate=cert,
+    acceptableProtocols=[b'h2'],
+)
 
 if __name__ == '__main__':
     argsGetter = ConfigurationGetter()
@@ -33,8 +47,9 @@ if __name__ == '__main__':
         args.max_username_length,
         args.stats_db_file
     )
-    endpoint4 = TCP4ServerEndpoint(reactor, int(args.port))
+    #endpoint4 = TCP4ServerEndpoint(reactor, int(args.port))
+    endpoint4 = SSL4ServerEndpoint(reactor, int(args.port), options)
     endpoint4.listen(factory)
-    endpoint6 = TCP6ServerEndpoint(reactor, int(args.port))
-    endpoint6.listen(factory)
+    #endpoint6 = TCP6ServerEndpoint(reactor, int(args.port))
+    #endpoint6.listen(factory)
     reactor.run()
