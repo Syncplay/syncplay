81e0a2004745120c219d54526be6107b769e5c3c
Alberto Sottile <alby128@gmail.com>
2019-02-04 17:50:07 +0100

    startTLS: avoid retrying TLS connection if hostname is an IP address

--- syncplay/protocols.py
+++ syncplay/protocols.py
@@ -81,16 +81,13 @@ class SyncClientProtocol(JSONCommandProtocol):
             self.sendTLS({"startTLS": "send"})
             self._client.ui.showMessage("Attempting secure connection")
         else:
+            self._client.ui.showErrorMessage("This server does not support TLS")
             self.sendHello()
 
     def connectionLost(self, reason):
         try:
             if "Invalid DNS-ID" in str(reason.value):
                 self._client._serverSupportsTLS = False
-            elif "certificate verify failed" in str(reason.value):
-                self._client._serverSupportsTLS = False
-            elif "tlsv1 alert protocol version" in str(reason.value):
-                self._client._clientSupportsTLS = False
         except:
             pass
         self._client.destroyProtocol()
@@ -315,7 +312,6 @@ class SyncClientProtocol(JSONCommandProtocol):
 
     def handleError(self, error):
         if "startTLS" in error["message"] and not self.logged:
-            self._client.ui.showErrorMessage("This server does not support TLS")
             self._client._serverSupportsTLS = False
         else:
             self.dropWithError(error["message"])
