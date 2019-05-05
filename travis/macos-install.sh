de194d92ca239dddecc019a9fde8e25407476478
Alberto Sottile <alby128@gmail.com>
2019-05-05 22:07:48 +0200

    Travis: use pyside bottle from our tap

--- travis/macos-install.sh
+++ travis/macos-install.sh
@@ -6,8 +6,7 @@ which python3
 python3 --version
 which pip3
 pip3 --version
-curl -L https://raw.githubusercontent.com/Homebrew/homebrew-core/dd6c67c1ba664c8910fe96aeb58f9938d97d9a53/Formula/pyside.rb -o pyside.rb
-brew install ./pyside.rb
+brew install albertosottile/syncplay/pyside
 python3 -c "from PySide2 import __version__; print(__version__)"
 python3 -c "from PySide2.QtCore import __version__; print(__version__)" 
 pip3 install py2app
