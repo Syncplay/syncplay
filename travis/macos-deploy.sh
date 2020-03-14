94637bb65d900ade830f24c65336b04f58bc707c
Tom Parker-Shemilt <palfrey@tevp.net>
2020-03-14 14:24:35 +0000

    Skip the openssl workaround now we're no longer upgrading openssl

--- travis/macos-deploy.sh
+++ travis/macos-deploy.sh
@@ -11,9 +11,3 @@ mkdir dist/Syncplay.app/Contents/Resources/es_419.lproj
 pip3 install dmgbuild
 mv syncplay/resources/macOS_readme.pdf syncplay/resources/.macOS_readme.pdf
 dmgbuild -s appdmg.py "Syncplay" dist_bintray/Syncplay_${VER}.dmg
-
-# Workaround for deployment issues with newer openssl.
-# See https://travis-ci.community/t/ruby-openssl-python-deployment-fails-on-osx-image/6753/9
-for lib in ssl crypto; do ln -s /usr/local/opt/openssl{@1.0,}/lib/lib${lib}.1.0.0.dylib; done
-rvm reinstall $(travis_internal_ruby) --disable-binary
-for lib in ssl crypto; do rm /usr/local/opt/openssl/lib/lib${lib}.1.0.0.dylib; done
