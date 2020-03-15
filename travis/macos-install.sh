68ca4c1b6050b47821c51e13abee4e1e631ff503
Tom Parker-Shemilt <palfrey@tevp.net>
2020-03-15 12:06:26 +0000

    Need to explicitly upgrade Python

--- travis/macos-install.sh
+++ travis/macos-install.sh
@@ -5,7 +5,7 @@ set -ex
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 
 # Python 3.7.4 with 10.12 bottle
-brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/e9004bd764c9436750a50e0b428548f68fe6a38a/Formula/python.rb
+brew upgrade https://raw.githubusercontent.com/Homebrew/homebrew-core/e9004bd764c9436750a50e0b428548f68fe6a38a/Formula/python.rb
 
 which python3
 python3 --version
