f85741988d4a1bb91ceb84d73b4a79e97ae23931
Tom Parker-Shemilt <palfrey@tevp.net>
2020-03-15 10:35:54 +0000

    Reset back to custom tap pyside

--- travis/macos-install.sh
+++ travis/macos-install.sh
@@ -15,7 +15,7 @@ which python3
 python3 --version
 which pip3
 pip3 --version
-brew install pyside
+brew install albertosottile/syncplay/pyside
 python3 -c "from PySide2 import __version__; print(__version__)"
 python3 -c "from PySide2.QtCore import __version__; print(__version__)" 
 pip3 install py2app
