8dda01328f421fcb8d7d1413290219bdd75343e7
Etoh <etoh@syncplay.pl>
2020-05-12 19:33:07 +0100

    Update mpv compatibility message

--- syncplay/players/mpv.py
+++ syncplay/players/mpv.py
@@ -41,7 +41,7 @@ class MpvPlayer(BasePlayer):
             the_reactor = reactor
             the_reactor.callFromThread(client.ui.showErrorMessage,
                                         "This version of mpv is not compatible with Syncplay. "
-                                        "Please use mpv >=0.29.0.", True)
+                                        "Please use mpv >=0.23.0.", True)
             the_reactor.callFromThread(client.stop)
             return
 
@@ -49,7 +49,7 @@ class MpvPlayer(BasePlayer):
         if not constants.MPV_OSC_VISIBILITY_CHANGE_VERSION:
             client.ui.showDebugMessage(
                 "This version of mpv is not known to be compatible with changing the OSC visibility. "
-                "Please use mpv >=0.28.0.")
+                "Please use mpv >=0.23.0.")
         return MpvPlayer(client, MpvPlayer.getExpandedPath(playerPath), filePath, args)
 
     @staticmethod
