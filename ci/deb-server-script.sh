ef4b6b31f546c81dd5de3e6a8d4b7199ec0f7690
Daniel Wróbel <wrobel.dan@gmail.com>
2021-01-23 20:21:24 +0100

    Allow running the server build in the same job.
    Making the paths used by server package build process separate from full package.

--- ci/deb-server-script.sh
+++ ci/deb-server-script.sh
@@ -3,7 +3,7 @@
 set -x
 set -e
 
-mkdir -p /tmp/syncplay/DEBIAN
+mkdir -p /tmp/syncplay-server/DEBIAN
 
 echo "Package: syncplay-server
 Version: "$(
@@ -24,12 +24,12 @@ Priority: optional
 Description: Server only package.
  Solution to synchronize video playback across multiple instances of mpv, VLC, MPC-HC and MPC-BE over the Internet.
  Syncplay synchronises the position and play state of multiple media players so that the viewers can watch the same thing at the same time. This means that when one person pauses/unpauses playback or seeks (jumps position) within their media player then this will be replicated across all media players connected to the same server and in the same 'room' (viewing session). When a new person joins they will also be synchronised. Syncplay also includes text-based chat so you can discuss a video as you watch it (or you could use third-party Voice over IP software to talk over a video)." \
-> /tmp/syncplay/DEBIAN/control
+> /tmp/syncplay-server/DEBIAN/control
 echo "#!/bin/sh
-py3clean -p syncplay
+py3clean -p syncplay-server
 "
-> /tmp/syncplay/DEBIAN/prerm
-chmod 555 /tmp/syncplay/DEBIAN/prerm
+> /tmp/syncplay-server/DEBIAN/prerm
+chmod 555 /tmp/syncplay-server/DEBIAN/prerm
 
-make install-server DESTDIR=/tmp/syncplay
-dpkg -b /tmp/syncplay/
+make install-server DESTDIR=/tmp/syncplay-server
+dpkg -b /tmp/syncplay-server/
