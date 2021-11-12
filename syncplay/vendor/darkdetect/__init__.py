2070d99f019b79d23540e5f2354e65e3b496bea0
Alberto Sottile <asottile@gmail.com>
2021-11-12 23:10:21 +0100

    darkdetect: update vendor copy to 0.5.2

--- syncplay/vendor/darkdetect/__init__.py
+++ syncplay/vendor/darkdetect/__init__.py
@@ -4,7 +4,7 @@
 #  Distributed under the terms of the 3-clause BSD License.
 #-----------------------------------------------------------------------------
 
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 
 import sys
 import platform
@@ -19,7 +19,7 @@ if sys.platform == "darwin":
 elif sys.platform == "win32" and int(platform.release()) >= 10:
     # Checks if running Windows 10 version 10.0.14393 (Anniversary Update) OR HIGHER. The getwindowsversion method returns a tuple.
     # The third item is the build number that we can use to check if the user has a new enough version of Windows.
-    winver = int(sys.getwindowsversion()[2])
+    winver = int(platform.version().split('.')[2])
     if winver >= 14393:
         from ._windows_detect import *
     else:
