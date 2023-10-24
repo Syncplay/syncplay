faf2a6081aebec6fa1445897fa8b7f94abb22f40
Etoh <etoh@syncplay.pl>
2023-10-24 20:05:19 +0100

    Update ep_client.py

--- syncplay/ep_client.py
+++ syncplay/ep_client.py
@@ -1,9 +1,20 @@
 import sys
+import ctypes
 
 from syncplay.clientManager import SyncplayClientManager
-from syncplay.utils import blackholeStdoutForFrozenWindow
+from syncplay.utils import blackholeStdoutForFrozenWindow, isWindows
 
-def main():
+def main():   
+    def doWindowsRedistCheck():
+        try:
+            ctypes.CDLL('vcruntime140.dll')
+        except OSError:
+            ctypes.windll.user32.MessageBoxW(0, '''Syncplay relies on the Microsoft Visual C++ Redistributable which is not installed.
+                 It can be downloaded at https://aka.ms/vs/17/release/vc_redist.x86.exe
+                 For more details see http://syncplay.pl/''', "Syncplay", 1)
+        sys.exit()
+    if isWindows():
+        doWindowsRedistCheck()
     blackholeStdoutForFrozenWindow()
     SyncplayClientManager().run()
 
