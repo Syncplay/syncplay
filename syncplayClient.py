e26ad92629cf32337aab455812b7b935378b5fb3
Etoh <etoh@syncplay.pl>
2023-10-24 18:31:15 +0100

    Update syncplayClient.py
    Check for VC redistibutable on Windows

--- syncplayClient.py
+++ syncplayClient.py
@@ -14,4 +14,20 @@ except AttributeError:
 from syncplay import ep_client
 
 if __name__ == '__main__':
+    def isWindows():
+        return sys.platform.startswith(constants.OS_WINDOWS)
+
+    def doWindowsRedistCheck():
+        import ctypes
+        try:
+            ctypes.CDLL('vcruntime140.dll')
+        except OSError:
+            ctypes.windll.user32.MessageBoxW(0, '''Syncplay relies on the Microsoft Visual C++ Redistributable which is not installed.
+                 It can be downloaded at https://aka.ms/vs/17/release/vc_redist.x86.exe
+                 For more details see httsp://syncplay.pl/''', "Syncplay", 1)
+        sys.exit()
+
+    if isWindows():
+        doWindowsRedistCheck()
+        
     ep_client.main()
