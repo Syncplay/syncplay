0a7753d97342334908f5ea0d30e76cf0bb36664a
Eyal Mazuz <mazuzeyal@protonmail.com>
2025-03-24 18:11:41 +0200

    removed useless print statement
    Signed-off-by: Eyal Mazuz <mazuzeyal@protonmail.com>

--- syncplay/players/memento.py
+++ syncplay/players/memento.py
@@ -3,13 +3,15 @@ import os
 from syncplay import constants
 
 from syncplay.players.mpv import MpvPlayer
-from syncplay.utils import findResourcePath, playerPathExists 
+from syncplay.utils import findResourcePath, playerPathExists
+
 
 class MementoPlayer(MpvPlayer):
-    
     @staticmethod
     def run(client, playerPath, filePath, args):
-            return MementoPlayer(client, MementoPlayer.getExpandedPath(playerPath), filePath, args)
+        return MementoPlayer(
+            client, MementoPlayer.getExpandedPath(playerPath), filePath, args
+        )
 
     @staticmethod
     def getDefaultPlayerPathsList():
@@ -20,13 +22,11 @@ class MementoPlayer(MpvPlayer):
                 l.append(p)
         return l
 
-
     @staticmethod
     def isValidPlayerPath(path):
         if "memento" in path and MementoPlayer.getExpandedPath(path):
             return True
         return False
-
 
     @staticmethod
     def getExpandedPath(playerPath):
@@ -39,26 +39,24 @@ class MementoPlayer(MpvPlayer):
                 return playerPath
         if os.access(playerPath, os.X_OK):
             return playerPath
-        for path in os.environ['PATH'].split(':'):
+        for path in os.environ["PATH"].split(":"):
             path = os.path.join(os.path.realpath(path), playerPath)
             if os.access(path, os.X_OK):
                 return path
-
 
     @staticmethod
     def getIconPath(path):
         return constants.MEMENTO_ICONPATH
 
-    
     @staticmethod
     def getStartupArgs(userArgs):
         args = constants.MPV_ARGS
         args["scripts"] = findResourcePath("syncplayintf.lua")
         if userArgs:
             for argToAdd in userArgs:
-                if argToAdd.startswith('--'):
+                if argToAdd.startswith("--"):
                     argToAdd = argToAdd[2:]
-                elif argToAdd.startswith('-'):
+                elif argToAdd.startswith("-"):
                     argToAdd = argToAdd[1:]
                 if argToAdd.strip() == "":
                     continue
@@ -70,5 +68,4 @@ class MementoPlayer(MpvPlayer):
                     argName = argToAdd
                     argValue = ""
                 args[argName] = argValue
-        print(f"{args=}")
         return args
